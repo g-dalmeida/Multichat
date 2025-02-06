5a004c99035a9e7fd00a455e30088da9bc9a4fe9
g-dalmeida <giordix23@gmail.com>
2025-02-06 09:07:14 +0100

    Finito

--- src/ThreadRicevi.java
+++ src/ThreadRicevi.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.io.InputStreamReader;
 
-public class ThreadRicevi {
+public class ThreadRicevi implements Runnable{
+    private Socket socket;
+   BufferedReader in;
+
+   public ThreadRicevi(Socket socket) throws IOException{
+       this.socket = socket;
+       in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+   }
+
+   public void run(){
+       String messaggio;
+       try{
+           messaggio= in.readLine();
+           while (messaggio != null){
+               System.out.println(messaggio);
+               messaggio = in.readLine();
+           }
+           System.out.println("Server chiuso");
+           socket.close();
+       }catch (IOException e){
+           System.out.println("Errore di connessione");
+       }
+   }
 }
